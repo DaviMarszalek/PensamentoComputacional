8c40d7b635033f5d73eb377f61c1331a66bd3515
Davi Marszalek <davi.c.m.rocha@gmail.com>
2025-05-07 21:52:26 -0300

    07/05

--- Classes_Objetos_Python/ContaBancaria/main.py
+++ Classes_Objetos_Python/ContaBancaria/main.py
@@ -1,13 +1,42 @@
 from models.contabancaria import ContaBancaria
 
+'''
+- 1 Criar conta
+- 2 Sacar
+- 3 Depositar
+- 4 Transferir
+- 5 Exibir saldo
+- 6 Exibir histórico
+- 7 Excluir conta
+'''
 
-guilherme = ContaBancaria("Guilherme", 1000, 500, [])
-pedro = ContaBancaria("Pedro", 500, 200, [])
+banco = []
 
+a = int(input(f"Bem-vindo ao sistema de contas bancárias!\n \n1 - Criar conta\n2 - Sacar\n3 - Depositar\n4 - Transferir\n5 - Exibir saldo\n6 - Exibir histórico\n7 - Excluir conta \nEscolha uma opção: "))
+if a == 1:
+    titular = str(input("Digite o nome completo do titular da conta:"))
+    saldo = float(input("Digite o saldo inicial da conta: "))
+    limite = 200
+    if saldo > 0:
+        limite = limite + int(saldo / 2)
+    banco.append(ContaBancaria(titular, saldo, limite, [])) 
+    for conta in banco:
+        if conta.titular == titular:
+            print(f"{titular} tem R${conta.saldo} em conta e possui um limite de R${conta.limite}!") 
+if a == 2:
+    titular_saque = input("Digite o titular da conta que deseja sacar: ")
+    saque = float(input("Informe o valor do saque: "))
+    for conta in banco:
+        if conta.titular == titular_saque:
+            conta.sacar(saque)
+        else:
+            print(f"Conta com titular '{titular_saque}' não encontrada.")               
+    
 
+'''
+titular = input("Digite o titular da conta que deseja ver o saldo:")
 
-
-conta.depositar(150)
-conta.sacar(100)
-
-conta.exibir_historico()
+for conta in banco:
+    if conta.titular == titular:
+        print(f"O {titular} tem R${conta.saldo} em conta!")    
+'''    
--- Classes_Objetos_Python/ContaBancaria/models/contabancaria.py
+++ Classes_Objetos_Python/ContaBancaria/models/contabancaria.py
@@ -20,7 +20,7 @@ class ContaBancaria:
     def depositar(self, valor):
         '''
         Objetivo: Método que realiza o depósito de um valor na conta bancária.
-        Entrada: valor (float)
+        Entrada: valor (float).
         Return: True se a operação obtiver sucesso. False se a operação não for realizada.
         '''
         if valor > 0:
@@ -40,7 +40,7 @@ class ContaBancaria:
     def sacar(self, valor):
         '''
         Objetivo: Método que realiza o saque de um valor na conta bancária.
-        Entrada: valor (float)
+        Entrada: valor (float).
         Return: True se a operação obtiver sucesso. False se a operação não for realizada.
         '''
         if valor <= self.saldo:
@@ -67,28 +67,108 @@ class ContaBancaria:
         return False        
         
 
-    def transferir(self, transferencia, destinatario):
+
+
+    """
+    def depositar(self, valor, remetente = None):
         '''
-        Objetivo: Método que realiza a transferência de um valor entre contas bancárias.
-        Entrada: valor (float), destinatário (string)
+        Objetivo: Método que realiza o depósito de um valor na conta bancária.
+        Entradas: valor (float), remetente(string)
         Return: True se a operação obtiver sucesso. False se a operação não for realizada.
         '''
-        if transferencia <= self.saldo:
-            self.saldo -= transferencia
-            self.historico.append({"operacao": 2,
-                                   "remetente": self.titular,
-                                   "destinatario": destinatario,
-                                   "valor": transferencia,
+        op = 1
+        if remtente != None:
+            op = 2
+        if valor > 0:
+            self.saldo += valor
+            self.historico.append({"operacao": op,
+                                   "remetente": remtente,
+                                   "destinatario": self.titular,
+                                   "valor": valor,
                                    "saldo": self.saldo,
                                    "dataetempo": int(time.time())})
-            print(f"Transferência de R${transferencia} realizada com sucesso!")
+            print(f"Depósito de R${valor} realizado com sucesso!")
             return True
         else:
-           a = input(f"Deseja usar o limite de ({self.limite}) para sacar (R${transferencia}) ? (s para sim)")
+            print(f"Valor {valor} inválido para depósito.")   
+            return False 
+    
+    def sacar(self, valor, destinatario = None):
+        '''
+        Objetivo: Método que realiza o saque de um valor na conta bancária.
+        Entradas: valor (float), destinatario(string)
+        Return: True se a operação obtiver sucesso. False se a operação não for realizada.
+        '''
+        op = 0
+        if remtente != None:
+            op = 2
+        if valor <= self.saldo:
+            self.saldo -= valor
+            self.historico.append({"operacao": op,
+                                   "remetente": self.titular,
+                                   "destinatario": destinatario,
+                                   "valor": valor,
+                                   "saldo": self.saldo,
+                                   "dataetempo": int(time.time())})
+            print(f"Saque de R${valor} realizado com sucesso!")
+            return True
+        else:
+            a = input(f"Deseja usar o limite de ({self.limite}) para sacar (R${valor}) ? (s para sim)")
+            if a == 's':
+                if (self.saldo + self.limite) >= valor:
+                    self.saldo -= valor
+                    print(f"Saque de R${valor} realizado com sucesso!")
+                    return True
+                else:
+                    print("Saldo e limite insificientes para o saque!")
+            else:
+                print("Operação com limite cancelada.")        
+        return False 
+        
+        def transferir(self, destinatario, valor):
+         
+            '''
+            Objetivo: Método que realiza a transferência de um valor entre contas bancárias.
+            Entradas: destinatario (string), valor (float)
+            Return: True se a operação obtiver sucesso. False se a operação não for realizada.
+            ''' 
+            if self.sacar(valor, destinatario.titular)
+                destinatario.depositar(valor, self.titular)
+            """
+
+
+
+
+
+
+    def transferir(self, valor, destinatario):
+        '''
+        Objetivo: Método que realiza a transferência de um valor entre contas bancárias.
+        Entradas: valor (float), destinatário (string)
+        Return: True se a operação obtiver sucesso. False se a operação não for realizada.
+        '''
+        if valor <= self.saldo:
+            self.saldo -= valor
+            self.historico.append({"operacao": 2,
+                                   "remetente": self.titular,
+                                   "destinatario": destinatario.titular,
+                                   "valor": valor,
+                                   "saldo": self.saldo,
+                                   "dataetempo": int(time.time())})
+            print(f"Transferência de R${valor} realizada com sucesso!")
+            return True
+        else:
+           a = input(f"Deseja usar o limite de ({self.limite}) para sacar (R${valor}) ? (s para sim)")
            if a == 's':
-                if (self.saldo + self.limite) >= transferencia:
-                    self.saldo -= transferencia
-                    print(f"Transferência de R${transferencia} realizado com sucesso!")
+                if (self.saldo + self.limite) >= valor:
+                    self.saldo -= valor
+                    self.historico.append({"operacao": 2,
+                                   "remetente": self.titular,
+                                   "destinatario": destinatario.titular,
+                                   "valor": valor,
+                                   "saldo": self.saldo,
+                                   "dataetempo": int(time.time())})
+                    print(f"Transferência de R${valor} realizado com sucesso!")
                     return True
                 else:
                     print("Saldo e limite insificientes para o transferência!")
@@ -112,4 +192,5 @@ class ContaBancaria:
                 
                   
         
-    #def exibir_saldo(self)  
+    def exibir_saldo(self):  
+        print(f"Titular: {self.titular} - Saldo: {self.saldo} - Limite: {self.limite}")
